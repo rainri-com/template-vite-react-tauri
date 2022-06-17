2794b31d21f70678aceb7055418740e0c223084c
Com Rainri <rainri.com@gmail.com>
2022-06-17 11:24:15 +0900

    prettierの設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,25 +3,20 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "airbnb",
-    "airbnb-typescript",
-  ],
-  parser: "@typescript-eslint/parser",
+  extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended', 'airbnb', 'airbnb-typescript', 'prettier'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: "./tsconfig.json",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
-  plugins: ["react", "react-hooks", "@typescript-eslint"],
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
   rules: {
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "react/react-in-jsx-scope": "off",
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 'off',
   },
-};
+}
