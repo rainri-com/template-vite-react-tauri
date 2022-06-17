dea9ee8daf4fb313c9c7eca684f03ab228b0933a
Com Rainri <rainri.com@gmail.com>
2022-06-17 11:48:43 +0900

    styled-componentsの設定

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Reset } from 'styled-reset'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Reset />
     <App />
   </React.StrictMode>
 )
